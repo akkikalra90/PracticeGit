528aa7ecdb8bb3c11548b09dda093f2b07c6bbe3
akkikalra90 <akkikalra90@gmail.com>
2021-04-06 07:54:22 +0530

    Vehicle Class reference variable is ponting to Car Class Object i.e. Runtime Polymorphism

--- src/inheritance_inside_package/VehicleUse.java
+++ src/inheritance_inside_package/VehicleUse.java
@@ -3,13 +3,16 @@ package inheritance_inside_package;
 public class VehicleUse {
 
 	public static void main(String[] args) {
-		Vehicle v = new Vehicle(0);
-		v.print();
+//		Vehicle v = new Vehicle(0);
+//		v.print();
+//		
+//		Car c = new Car(105);
+//		c.name = "Honda City";
+//		c.numGears = 5;
+//		c.print();
 		
-		Car c = new Car(105);
-		c.name = "Honda City";
-		c.numGears = 5;
-		c.print();
+		Vehicle v = new Car(115);
+		v.print();
 		
 		return;
 	}
